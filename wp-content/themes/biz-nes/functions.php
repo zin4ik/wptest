7a50eee0af644f9343eae923bbb6800a5243b581
Serey <zin4ik86@gmail.com>
2019-11-10 19:31:00 +0200

    CREAT MENU HEADER

--- wp-content/themes/biz-nes/functions.php
+++ wp-content/themes/biz-nes/functions.php
@@ -35,7 +35,7 @@ if ( ! function_exists( 'biz_nes_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'biz-nes' ),
+			'pimary' => esc_html__( 'Primary', 'biz-nes' ),
 		) );
 
 		/*
--- wp-content/themes/biz-nes/header.php
+++ wp-content/themes/biz-nes/header.php
@@ -97,8 +97,26 @@
           
 
         </div>  <!-- /container -->
-
-
+      <?php 
+     wp_nav_menu([
+        'theme_location'  => 'pimary',
+        'menu'            => '', 
+        'container'       => 'div', 
+        'container_class' => '', 
+        'container_id'    => '',
+        'menu_class'      => 'menu', 
+        'menu_id'         => '',
+        'echo'            => true,
+        'fallback_cb'     => 'wp_page_menu',
+        'before'          => '',
+        'after'           => '',
+        'link_before'     => '',
+        'link_after'      => '',
+        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+        'depth'           => 0,
+        'walker'          => '',
+      ]);
+      ?>
         <div id="stuck_container" class="stuck_container">
           <div class="container">
             <nav class="nav">
