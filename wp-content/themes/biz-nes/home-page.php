8eec471e6ce96e31fbe9521689a39765a184f0b8
Serey <zin4ik86@gmail.com>
2019-11-14 22:22:40 +0200

    home page

--- wp-content/themes/biz-nes/home-page.php
+++ wp-content/themes/biz-nes/home-page.php
@@ -17,41 +17,34 @@ $services_main = get_post_meta($post->ID, 'home_page_services_list',true);
 
 
 	<main>
+<!-- ========= блок 1 банер========--> 
 	<?php if ($sliders_main && (get_post_meta($post->ID, 'home_page_slider_show',true) != 'off')) : ?>
-	
-
-
 		<section class="camera_container">
           <div id="camera" class="camera_wrap">
-
 	<?php	
-	
 	 foreach($sliders_main as $slider):
 		$slider_img=$slider[home_page_slider_upload] ? $slider[home_page_slider_upload]: '' ;
-		$slider_name=$slider[home_page_slider_name] ? $slider[home_page_slider_name]: '';
-	 
+		$slider_name=$slider[home_page_slider_name] ? $slider[home_page_slider_name]: ''; 
 	 ?>
-            <div data-src="<?php echo $slider_img;?>">
-              <div class="camera_caption fadeIn">
-                <div class="container">
-                  <div class="row">
-                    <div class="preffix_6 grid_6"><?php echo $slider_name;?></div>
-                  </div>
+      <div data-src="<?php echo $slider_img;?>">
+        <div class="camera_caption fadeIn">
+          <div class="container">
+            <div class="row">
+              <div class="preffix_6 grid_6"><?php echo $slider_name;?></div>
                 </div>
               </div>
             </div>
+          </div>
 			<?php endforeach;?>
 				</div>
 			</section>
 	 <?php endif;?>
 		  
-
-<!------------------ advansed -------------------->
+<!--========= блок 2 - advansed ============-->
 
 		  <section>
 		  <?php if ($advansed_main && (get_post_meta($post->ID, 'home_page_advansed_show',true) != 'off')) : ?>
-          <div class="container banner_wr">
-		  
+          <div class="container banner_wr">		  
             <ul class="banner">
 		<?php	
 		foreach($advansed_main as $advansed):
@@ -67,34 +60,30 @@ $services_main = get_post_meta($post->ID, 'home_page_services_list',true);
                 <a href="<?php echo $advansed_link?>"></a>
               </li>
 			<?php endforeach;?>
-            </ul>
-			
-          </div>
-		  
+            </ul>			
+          </div>		  
         </section>
 		<?php endif;?>
-<!-- =============product-list============== -->
+
+<!-- =============блок 3 - product-list============== -->
+
 		<section class="well ins1">
           <div class="container hr">
-            <ul class="row product-list">
-              <!-- <li class="grid_6"> -->
-             
+            <ul class="row product-list">            
           <?php 
           $delay_wow=0;
 				  foreach($services_main as $services_id):
 					$service = get_post( $services_id['home_page_services_post_type_select'], ARRAY_A );
-  // print_r($service);
           $delay_wow++;
 				 ?>
-
                 <li data-wow-delay="0.<?php echo $delay_wow?>s" class="grid_6 box wow fadeInRight product-list_services">  
                   <div  class="box_aside">
                   <?php 
                   if (get_post_meta($service['ID'], 'services_icon', true)):?>
- <!-- ============div icon============ -->
+  <!-- ============div icon============ -->
                   <div class="icon <?php echo get_post_meta($service['ID'], 'services_icon', true)?>"></div>
-<!-- ============/div icon============ -->
-<!-- ============/img============ -->
+  <!-- ============/div icon============ -->
+  <!-- ============/img============ -->
                   <?php else :
                   echo get_the_post_thumbnail( $service['ID'],'service_img' );
                   endif;?>
@@ -103,13 +92,108 @@ $services_main = get_post_meta($post->ID, 'home_page_services_list',true);
                   <div class="box_cnt__no-flow">
                     <h3><a href="<?php get_the_permalink($service['ID']);?>"><?php echo $service['post_title'];?></a></h3>
 		            	<p><?php echo wp_trim_words($service['post_content'], 10, '...');?></p>
-					
-					<!-- https://www.youtube.com/watch?v=9x4TglmhOBw -->
-
                   </div> <!--/box_cnt__no-flow-->
                 </li> <!--/box wow fadeInRight-->
 				 <?php endforeach;?>
             </ul>
+          </div>
+        </section>
+
+<!--============блок 4 про нас============-->
+
+        <section class="well1">
+          <div class="container">
+            <div class="row">
+              <div class="grid_4">
+                <h2>About</h2><img src="images/page-1_img01.jpg" alt="">
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatu. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p><a href="#" class="btn">Read more</a>
+              </div>
+              <div class="grid_4">
+                <h2>Services</h2>
+                <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+                <ul class="marked-list">
+                  <li><a href="#">Lorem ipsum dolor sit amet </a></li>
+                  <li><a href="#">Conse ctetur adipisicing</a></li>
+                  <li><a href="#">Elit sed do eiusmod tempor</a></li>
+                  <li><a href="#">Incididunt ut labore</a></li>
+                  <li><a href="#">Et dolore magna aliqua</a></li>
+                  <li><a href="#">Ut enim ad minim veniam</a></li>
+                  <li><a href="#">Quis nostrud exercitation</a></li>
+                  <li><a href="#">Incididunt ut labore</a></li>
+                  <li><a href="#">Et dolore magna aliqua</a></li>
+                </ul><a href="#" class="btn">Read more</a>
+              </div>
+              <div class="grid_4">
+                <div class="info-box">
+                  <h2 class="fa-comment">Help center</h2>
+                  <hr>
+                  <h3>Ask professionals:</h3>
+                  <dl>
+                    <dt>Monday - Friday:</dt>
+                    <dd>8am-7pm</dd>
+                  </dl>
+                  <dl>
+                    <dt>Saturday:</dt>
+                    <dd>8am-5pm</dd>
+                  </dl>
+                  <dl>
+                    <dt>Sunday:</dt>
+                    <dd>1pm-5pm</dd>
+                  </dl>
+                  <hr>
+                  <h3>24/7 Online Support:</h3>
+                  <dl>
+                    <dt>800-2345-6789</dt>
+                  </dl>
+                </div>
+                <div class="owl-carousel">
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img02.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img03.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img04.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                  <div class="item">
+                    <blockquote class="box">
+                      <div class="box_aside"><img src="images/page-1_img05.jpg" alt=""></div>
+                      <div class="box_cnt__no-flow">
+                        <p>
+                          <q>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</q>
+                        </p>
+                        <cite><a href="#">Incididunt ut labor</a></cite>
+                      </div>
+                    </blockquote>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 		</main><!-- #main -->
--- wp-content/themes/biz-nes/index.php
+++ wp-content/themes/biz-nes/index.php
@@ -78,54 +78,7 @@ get_header();
             </ul>
           </div>
         </section> -->
-        <section class="well ins1">
-          <div class="container hr">
-            <ul class="row product-list">
-              <li class="grid_6">
-                <div class="box wow fadeInRight">
-                  <div class="box_aside">
-                    <div class="icon fa-comments"></div>
-                  </div>
-                  <div class="box_cnt__no-flow">
-                    <h3><a href="#">Incididunt ut labore et dolore</a></h3>
-                    <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
-                  </div>
-                </div>
-                <hr>
-                <div data-wow-delay="0.2s" class="box wow fadeInRight">
-                  <div class="box_aside">
-                    <div class="icon fa-calendar-o"></div>
-                  </div>
-                  <div class="box_cnt__no-flow">
-                    <h3><a href="#">Incididunt ut labore et dolore</a></h3>
-                    <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
-                  </div>
-                </div>
-              </li>
-              <li class="grid_6">
-                <div data-wow-delay="0.3s" class="box wow fadeInRight">
-                  <div class="box_aside">
-                    <div class="icon fa-group"></div>
-                  </div>
-                  <div class="box_cnt__no-flow">
-                    <h3><a href="#">Incididunt ut labore et dolore</a></h3>
-                    <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
-                  </div>
-                </div>
-                <hr>
-                <div data-wow-delay="0.4s" class="box wow fadeInRight">
-                  <div class="box_aside">
-                    <div class="icon fa-thumbs-up"></div>
-                  </div>
-                  <div class="box_cnt__no-flow">
-                    <h3><a href="#">Incididunt ut labore et dolore</a></h3>
-                    <p>Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolor.</p>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
+       
         <section class="well1">
           <div class="container">
             <div class="row">
